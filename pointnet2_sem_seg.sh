de8c536d145f9e27ad2c55967c26666786f47489
L40S38 <82433665+L40S38@users.noreply.github.com>
2023-12-05 00:07:34 +0900

    part_seg executed

--- pointnet2_sem_seg.sh
+++ pointnet2_sem_seg.sh
@@ -5,5 +5,5 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate pointnet
 
 # ## e.g., pointnet2_ssg without normal features
-python train_semseg.py --gpu 7 --model pointnet2_sem_seg --log_dir pointnet2_sem_seg
-python test_semseg.py --gpu 7 --log_dir pointnet2_sem_seg 
+#python train_semseg.py --gpu 7 --model pointnet2_sem_seg --log_dir pointnet2_sem_seg
+python test_semseg.py --gpu 7 --log_dir pointnet2_sem_seg --visual
